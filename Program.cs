f22672871ff8c0ca82424a799ce7a51a07de5a5d
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Air.local>
2025-02-25 18:18:20 -0700

    Finished Step 3

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 ﻿// Benjamin Johnson, 2/25, Lab-3
+using System.Net;
+
 Console.Clear();
 
-// Greeting
+// Greeting - Step #2
 Console.WriteLine(@"
 ----------------------------------------------------------------------
 |                             Mastermind                             |
@@ -9,3 +11,41 @@ Console.WriteLine(@"
 |    'a' and 'g' and have arranged them in a particular order.       |
 |  Your job is to guess the letters and put them in the right order. |
 ----------------------------------------------------------------------");
+
+// Creating the secret word - Step #3
+string secret = "";
+Random rand = new Random();
+
+for (int i = 0; i < 4; i++)
+{
+    char temp = (char) rand.Next(97,104);
+    while(true)
+    {
+        if (secret.Contains(temp))
+            temp = (char) rand.Next(97,104);
+        else
+        {
+            secret += temp;
+            break;
+        }
+    }  
+}
+Console.WriteLine(secret); // Written for debugging
+
+// Prompting the User to guess the word - Step #3
+int guessNumber = 0;
+bool boolean = true;
+
+do 
+{
+    guessNumber++;
+    Console.WriteLine($"Guess #{guessNumber}: Please guess a sequence of 4 lowercase letters with no repeats.");
+    string? guess = Console.ReadLine();
+    if (secret[0] == guess[0] && secret[1] == guess[1] && secret[2] == guess[2] && secret[3] == guess[3])
+    {   
+        boolean = false;
+    }
+}
+while (boolean);
+
+Console.Write("You won");
--- obj/Debug/net9.0/lab-3-mastermind-BenjaminJ-146.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-3-mastermind-BenjaminJ-146.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-3-mastermind-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3e2250ecaa5b416794ffd3d3c6ecf8c09461bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b75b5da084b590d7f28ed6081dd925581c21d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-3-mastermind-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-3-mastermind-BenjaminJ-146")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
